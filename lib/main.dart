52dc6b0b06eb4c7608b1dec43aa6c7467c0c345c
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-12 20:34:11 -0300

    feat: initial setup of project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram App Challenge',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Instagram Clone'),
+      ),
+    );
+  }
+}
