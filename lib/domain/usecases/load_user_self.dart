e57a840e71470fe3622ea3b3aa90048c12930af8
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 14:15:14 -0300

    feat: load current user usecase

--- lib/domain/usecases/load_user_self.dart
+++ lib/domain/usecases/load_user_self.dart
@@ -1,5 +1,5 @@
 import '../entities/entities.dart';
 
-abstract class LoadCurrentUser {
+abstract class LoadUserSelf {
   Future<UserEntity> load(String userUID);
 }
--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,2 +1,3 @@
+export './load_current_user.dart';
 export './load_posts.dart';
-export './load_user.dart';
+export './load_user_self.dart';
