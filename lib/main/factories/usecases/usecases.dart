eced6dcb112fad64f1874fa25a6860a4d97a2752
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 18:02:42 -0300

    feat: authentication and save current user usecase

--- lib/main/factories/usecases/usecases.dart
+++ lib/main/factories/usecases/usecases.dart
@@ -1,3 +1,5 @@
+export './authentication_factory.dart';
 export './load_current_user_factory.dart';
 export './load_posts_factory.dart';
 export './load_user_self_factory.dart';
+export './save_current_factory.dart';
