ff0be0656ebb01e3cb459f546cb1be8e73745a8a
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 14:20:05 -0300

    feat: load local current user factory

--- lib/main/factories/usecases/load_user_self_factory.dart
+++ lib/main/factories/usecases/load_user_self_factory.dart
@@ -2,6 +2,6 @@ import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usecases.dart';
 import '../factories.dart';
 
-LoadCurrentUser makeLocalLoadCurrentUser() => LocalLoadCurrentUser(
-      fetchSecureCacheStorage: makeSecureStorageAdapter(),
+LoadUserSelf makeRemoteLoadUserSelf() => RemoteLoadUserSelf(
+      cloudFirestore: makeCloudFirestore(),
     );
--- lib/main/factories/usecases/usecases.dart
+++ lib/main/factories/usecases/usecases.dart
@@ -1,2 +1,3 @@
 export './load_current_user_factory.dart';
 export './load_posts_factory.dart';
+export './load_user_self_factory.dart';
