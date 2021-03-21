384bd8d042cd3f194a1e970033f9db472b16fea3
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-20 22:33:52 -0300

    feat: user viewmodel

---
+++ lib/ui/pages/home/user_viewmodel.dart
@@ -0,0 +1,13 @@
+class UserViewModel {
+  final String uid;
+  final String username;
+  final String avatar;
+  final String name;
+
+  const UserViewModel({
+    required this.uid,
+    required this.username,
+    required this.avatar,
+    required this.name,
+  });
+}
