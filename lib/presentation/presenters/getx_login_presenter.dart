8c6ea0b7c5a04c347e0af5f0106115914f63d69c
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 17:57:47 -0300

    feat: GetxLoginPresenter

---
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -0,0 +1,92 @@
+import 'package:get/get.dart';
+
+import '../../ui/helpers/helpers.dart';
+import '../../ui/pages/pages.dart';
+import '../../domain/helpers/helpers.dart';
+import '../../domain/usecases/usecases.dart';
+import '../protocols/protocols.dart';
+import '../mixins/mixins.dart';
+
+class GetxLoginPresenter extends GetxController
+    with LoadingManager, NavigationManager, FormManager, UIErrorManager
+    implements LoginPresenter {
+  final Validation validation;
+  final Authentication authentication;
+  final SaveCurrentUser saveCurrentUser;
+
+  final _emailError = Rx<UIError>();
+  final _passwordError = Rx<UIError>();
+
+  String? _email;
+  String? _password;
+
+  Stream<UIError?> get emailErrorStream => _emailError.stream;
+  Stream<UIError?> get passwordErrorStream => _passwordError.stream;
+
+  GetxLoginPresenter({
+    required this.validation,
+    required this.authentication,
+    required this.saveCurrentUser,
+  });
+
+  void validateEmail(String email) {
+    _email = email;
+    _emailError.value = _validateField('email');
+    _validateForm();
+  }
+
+  void validatePassword(String password) {
+    _password = password;
+    _passwordError.value = _validateField('password');
+    _validateForm();
+  }
+
+  UIError? _validateField(String field) {
+    final formData = {
+      'email': _email,
+      'password': _password,
+    };
+    final error = validation.validate(field: field, input: formData);
+    switch (error) {
+      case ValidationError.invalidField:
+        return UIError.invalidField;
+      case ValidationError.requiredField:
+        return UIError.requiredField;
+      case ValidationError.noError:
+        return null;
+    }
+  }
+
+  void _validateForm() {
+    isFormValid = _emailError.value == null &&
+        _passwordError.value == null &&
+        _email != null &&
+        _password != null;
+  }
+
+  Future<void> auth() async {
+    try {
+      mainError = null;
+      isLoading = true;
+      final userUID = await authentication.auth(
+        AuthenticationParams(email: _email!, password: _password!),
+      );
+      await saveCurrentUser.save(userUID);
+      navigateTo = '/home';
+    } on DomainError catch (error) {
+      switch (error) {
+        case DomainError.invalidCredentials:
+          mainError = UIError.invalidCredentials;
+          break;
+        default:
+          mainError = UIError.unexpected;
+          break;
+      }
+      isLoading = false;
+    }
+  }
+
+  void goToSignUp() {
+    navigateTo = '/signup';
+  }
+}
