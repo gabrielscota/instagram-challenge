b674d18409c3bff7e1ecfb2ce9802d9b2f4fe087
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-20 22:03:13 -0300

    feat: GetxHomePresenter factory

--- lib/presentation/presenters/getx_home_presenter.dart
+++ lib/presentation/presenters/getx_home_presenter.dart
@@ -1,17 +1,20 @@
 import 'package:get/get.dart';
 
+import '../../domain/entities/entities.dart';
 import '../../domain/usecases/usecases.dart';
 import '../../ui/pages/pages.dart';
 
 class GetxHomePresenter extends GetxController implements HomePresenter {
   final LoadPosts loadPosts;
+  final LoadCurrentUser loadCurrentUser;
 
-  GetxHomePresenter({required this.loadPosts});
+  GetxHomePresenter({required this.loadPosts, required this.loadCurrentUser});
 
   @override
-  Future<void> loadPostsData() async {
+  Future<void> loadPostsData(String userUID) async {
     try {
-      await loadPosts.load();
+      final UserEntity user = await loadCurrentUser.load(userUID);
+      await loadPosts.load(user.uid);
     } catch (_) {}
   }
 }
