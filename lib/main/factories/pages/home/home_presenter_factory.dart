98c0673d2ba08792b90601c2d30f88fc3cd01c0d
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-20 22:02:35 -0300

    feat: home page factory

---
+++ lib/main/factories/pages/home/home_presenter_factory.dart
@@ -0,0 +1,8 @@
+import '../../../../presentation/presenters/presenters.dart';
+import '../../../../ui/pages/pages.dart';
+import '../../factories.dart';
+
+HomePresenter makeGetxHomePresenter() => GetxHomePresenter(
+      loadPosts: makeRemoteLoadPosts(),
+      loadCurrentUser: makeRemoteLoadCurrentUser(),
+    );
