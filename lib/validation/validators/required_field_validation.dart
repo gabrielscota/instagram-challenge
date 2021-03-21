e885145004b4d3814366d1f6451d264910bdc02b
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 15:01:30 -0300

    feat: added validation to required field

---
+++ lib/validation/validators/required_field_validation.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+
+import '../../presentation/protocols/protocols.dart';
+import '../protocols/protocols.dart';
+
+class RequiredFieldValidation extends Equatable implements FieldValidation {
+  final String field;
+
+  List get props => [field];
+
+  RequiredFieldValidation(this.field);
+
+  ValidationError validate(Map input) =>
+      input[field]?.isNotEmpty == true ? null : ValidationError.requiredField;
+}
