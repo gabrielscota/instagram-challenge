2d01cc8c8c9692f4b294d1df9deed56cb636bbd6
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 18:07:50 -0300

    feat: save current user usecase in domain layer

---
+++ lib/domain/usecases/save_current_user.dart
@@ -0,0 +1,3 @@
+abstract class SaveCurrentUser {
+  Future<void> save(String? user);
+}
--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,3 +1,5 @@
+export './authentication.dart';
 export './load_current_user.dart';
 export './load_posts.dart';
 export './load_user_self.dart';
+export './save_current_user.dart';
