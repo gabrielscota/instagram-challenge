6f398aee9d5d078729bb2ae4c91ea99c81ce6ca1
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 17:56:01 -0300

    feat: pt_br translation and strings

---
+++ lib/ui/helpers/i18n/resources.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+import './strings/strings.dart';
+
+class R {
+  static Translation string = PtBr();
+
+  static void load(Locale locale) {
+    switch (locale.toString()) {
+      default:
+        string = PtBr();
+        break;
+    }
+  }
+}
