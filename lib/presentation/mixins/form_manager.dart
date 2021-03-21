a3325c17ab0d6b014951b210de98f3ad8daf202a
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 17:59:47 -0300

    feat: form manager to control valid form

---
+++ lib/presentation/mixins/form_manager.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+mixin FormManager on GetxController {
+  final RxBool _isFormValid = false.obs;
+  Stream<bool?> get isFormValidStream => _isFormValid.stream;
+  set isFormValid(bool value) => _isFormValid.value = value;
+}
