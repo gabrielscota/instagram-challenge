2f0e0ef8027e159583450f356d2a6653688360fd
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-22 00:59:29 -0300

    fix: now password min length is 6

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -9,5 +9,5 @@ Validation makeLoginValidation() => ValidationComposite(
 
 List<FieldValidation> makeLoginValidations() => [
       ...ValidationBuilder.field('email').required().email().build(),
-      ...ValidationBuilder.field('password').required().min(3).build()
+      ...ValidationBuilder.field('password').required().min(6).build()
     ];
