82314ebd46dda6b3371672b3a749c12c49a206d1
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-21 23:41:34 -0300

    feat: animation in login button

--- lib/ui/pages/login/components/login_button.dart
+++ lib/ui/pages/login/components/login_button.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_challenge/ui/mixins/mixins.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 import '../../../helpers/helpers.dart';
 import '../login.dart';
 
-class LoginButton extends StatelessWidget {
+class LoginButton extends StatelessWidget with KeyboardManager {
   @override
   Widget build(BuildContext context) {
     final presenter = Provider.of<LoginPresenter>(context);
 
-    return StreamBuilder<bool?>(
-      stream: presenter.isFormValidStream,
-      builder: (context, snapshot) {
-        return ElevatedButton(
-          onPressed: snapshot.data == true ? presenter.auth : null,
-          child: Text(R.string.enter.toUpperCase()),
-        );
-      },
+    return Container(
+      height: 52,
+      width: double.infinity,
+      child: StreamBuilder<bool?>(
+        stream: presenter.isFormValidStream,
+        builder: (context, snapshot) {
+          return ElevatedButton(
+            onPressed: snapshot.data == true
+                ? () {
+                    hideKeyboard(context);
+                    presenter.auth();
+                  }
+                : null,
+            child: StreamBuilder<bool?>(
+              stream: presenter.isLoadingStream,
+              builder: (context, snapshot) {
+                if (snapshot.data == true) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: LottieBuilder.asset(
+                      'lib/ui/assets/animations/loading-white.json',
+                      repeat: true,
+                      fit: BoxFit.fitHeight,
+                    ),
+                  );
+                } else {
+                  return Text(R.string.enter.toUpperCase());
+                }
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -6,8 +6,7 @@ import '../../mixins/mixins.dart';
 import '../pages.dart';
 import './components/components.dart';
 
-class LoginPage extends StatelessWidget
-    with KeyboardManager, UIErrorManager, NavigationManager {
+class LoginPage extends StatelessWidget with KeyboardManager, UIErrorManager, NavigationManager {
   final LoginPresenter presenter;
 
   LoginPage({required this.presenter});
@@ -17,38 +16,33 @@ class LoginPage extends StatelessWidget
     return Scaffold(
       body: Builder(
         builder: (context) {
+          handleMainError(context, presenter.mainErrorStream);
           handleNavigation(presenter.navigateToStream, clear: true);
 
           return GestureDetector(
             onTap: () => hideKeyboard(context),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(32),
-                    child: Provider<LoginPresenter>(
-                      create: (_) => presenter,
-                      child: Form(
-                        child: Column(
-                          children: <Widget>[
-                            EmailInput(),
-                            Padding(
-                              padding: EdgeInsets.only(top: 8, bottom: 32),
-                              child: PasswordInput(),
-                            ),
-                            LoginButton(),
-                            TextButton.icon(
-                              onPressed: presenter.goToSignUp,
-                              icon: Icon(Icons.person),
-                              label: Text(R.string.addAccount),
-                            ),
-                          ],
-                        ),
+            child: Padding(
+              padding: EdgeInsets.all(32),
+              child: Provider<LoginPresenter>(
+                create: (_) => presenter,
+                child: Form(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      EmailInput(),
+                      Padding(
+                        padding: EdgeInsets.only(top: 8, bottom: 32),
+                        child: PasswordInput(),
                       ),
-                    ),
-                  )
-                ],
+                      LoginButton(),
+                      TextButton.icon(
+                        onPressed: presenter.goToSignUp,
+                        icon: Icon(Icons.person),
+                        label: Text(R.string.addAccount),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           );
