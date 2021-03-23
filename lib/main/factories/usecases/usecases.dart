b517d4d08a390af69793a363a88bfb9913f44d23
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-23 00:54:14 -0300

    chore: rename authentication usecase

--- lib/main/factories/usecases/usecases.dart
+++ lib/main/factories/usecases/usecases.dart
@@ -1,5 +1,7 @@
-export './authentication_factory.dart';
+export './delete_current_user_factory.dart';
 export './load_current_user_factory.dart';
 export './load_posts_factory.dart';
 export './load_user_self_factory.dart';
-export './save_current_factory.dart';
+export './save_current_user_factory.dart';
+export './user_authentication_factory.dart';
+export './user_logout_factory.dart';
