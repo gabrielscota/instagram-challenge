284d0c63a24b234c2bf0078478af1c78cd53e734
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-23 01:00:06 -0300

    feat: method to firebase logout function in data layer

--- lib/data/firebase/firebase_auth.dart
+++ lib/data/firebase/firebase_auth.dart
@@ -2,4 +2,5 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class FirebaseAuthentication {
   Future<UserCredential> authWithEmailAndPassword({required String email, required String password});
+  Future<void> logout();
 }
