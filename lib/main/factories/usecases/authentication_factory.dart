8f0f8ad3dd8dad1742814990bf249c86a2dec9c1
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-23 00:55:35 -0300

    feat: delete current user factory

--- lib/main/factories/usecases/authentication_factory.dart
+++
@@ -1,7 +0,0 @@
-import '../../../data/usecases/usecases.dart';
-import '../../../domain/usecases/usecases.dart';
-import '../factories.dart';
-
-Authentication makeRemoteAuthentication() => RemoteAuthentication(
-      firebaseAuthentication: makeFirebaseAuth(),
-    );
