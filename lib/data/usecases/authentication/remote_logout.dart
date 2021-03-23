10ba02099d07ffa55ced4a377d6206249b705324
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-23 00:58:56 -0300

    feat: remote logout usecase

---
+++ lib/data/usecases/authentication/remote_logout.dart
@@ -0,0 +1,17 @@
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../firebase/firebase.dart';
+
+class RemoteLogout implements UserLogout {
+  final FirebaseAuthentication firebaseAuthentication;
+
+  RemoteLogout({required this.firebaseAuthentication});
+
+  Future<void> logout() async {
+    try {
+      await firebaseAuthentication.logout();
+    } on FirebaseAuthError {
+      throw DomainError.unexpected;
+    }
+  }
+}
