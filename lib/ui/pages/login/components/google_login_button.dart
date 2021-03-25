23bb0ae1e787587258d0c23a9c9adf64de10feec
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-25 16:14:01 -0300

    feat: presenter dependencie to call method to google signin

--- lib/ui/pages/login/components/google_login_button.dart
+++ lib/ui/pages/login/components/google_login_button.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../pages/pages.dart';
+
 class GoogleLoginButton extends StatelessWidget {
+  final LoginPresenter presenter;
+
+  GoogleLoginButton({required this.presenter});
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: presenter.authWithGoogle,
       style: ElevatedButton.styleFrom(
         primary: Colors.white,
         padding: EdgeInsets.symmetric(vertical: 12.0),
