9eab487d7af2e05b273bff6aa743fcc67ad41d87
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-03-24 23:55:02 -0300

    feat: userSignUp usecase

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -5,3 +5,4 @@ export './load_user_self.dart';
 export './save_current_user.dart';
 export './user_authentication.dart';
 export './user_logout.dart';
+export './user_signup.dart';
---
+++ lib/domain/usecases/user_signup.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:instagram_challenge/domain/entities/entities.dart';
+
+abstract class UserSignUp {
+  Future<String?> signUp(SignUpParams params);
+}
+
+class SignUpParams extends Equatable {
+  final String email;
+  final String password;
+  final UserEntity user;
+
+  List get props => [email, user, password];
+
+  SignUpParams({required this.email, required this.user, required this.password});
+}
